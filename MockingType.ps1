2b7f97dc9d13ab56c8754e916f10ef7bbbcb5f5a
Taylor Sullivan <bondedtocrysti@gmail.com>
2018-02-23 22:32:23 -0700

    Adding initial scripts

---
+++ MockingType.ps1
@@ -0,0 +1,29 @@
+﻿Function Mocking-Type {
+    param(
+
+        [Parameter(Mandatory=$True,Position=0,HelpMessage="String to convert.")][ValidateLength(1,2048)][String]$ToMock,
+        [Parameter(Mandatory=$false,Position=1,HelpMessage="Number of lowercase letters between each upper case.  Defaults to 1.")][ValidateRange(0,5)][Int32]$spacing = 1
+
+    )
+
+    $array = $ToMock.ToString().ToLower().ToCharArray()
+    [String]$Output = ""
+    [Int32]$count = $spacing
+    
+    $array | ForEach-Object {
+        if ($_ -match '[a-z]') {
+            if ($count -eq $spacing) {
+                $count = 0
+                $output += $_.ToString().ToUpper()
+            } else {
+                $output += $_.ToString()
+                $count++
+            }
+        } else {
+            $output += $_.ToString()
+        }
+    }
+
+    Write-Host $Output
+    
+}
